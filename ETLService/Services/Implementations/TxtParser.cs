8a98f9615e768ff3c24047dd35fada1f8b26828f
Alex <oleksandr.tsisarenko@nure.ua>
2022-08-06 19:09:22 +0300

    Added txt parser

--- ETLService/Services/Implementations/TxtParser.cs
+++ ETLService/Services/Implementations/TxtParser.cs
@@ -1,6 +1,30 @@
-﻿namespace ETLService.Services.Implementations;
+﻿using ETLService.Models.InputModels;
+using ETLService.Services.Abstraction;
+using System.Linq;
 
-public class TxtParser
+namespace ETLService.Services.Implementations;
+
+public class TxtParser: Parser
 {
-    
+    private readonly ILogger _log;
+
+    public TxtParser(ILogger log)
+    {
+        _log = log;
+    }
+
+    public override string FileExtention => ".txt";
+
+    public override List<Transaction> ParseTransactions(
+        string path,
+        out int parsedLines,
+        out int errors)
+    {
+        using var sr = new StreamReader(path);
+        var fileData = sr.ReadToEnd();
+
+        _log.Info($"File read: {path} | Time: {DateTime.Now.ToLongTimeString()}");
+
+        return ParseFileData(fileData, out parsedLines, out errors);
+    }
 }

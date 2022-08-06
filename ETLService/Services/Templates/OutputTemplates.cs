ac9c94cc8629648366fd919fe0d357763f8abfe9
Alex <oleksandr.tsisarenko@nure.ua>
2022-08-06 18:59:53 +0300

    Added template for output

---
+++ ETLService/Services/Templates/OutputTemplates.cs
@@ -0,0 +1,10 @@
+﻿namespace ETLService.Services.Templates;
+
+public static class OutputTemplates
+{
+    public static string MetaLogTemplate =
+        "parsed_files: {0}\n" +
+        "parsed_lines: {1}\n" +
+        "found_errors: {2}\n" +
+        "invalid_files: [{3}]\n";
+}
